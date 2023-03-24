6e92ed86044e7994f6207556cea07c70fb5f33e1
migratezuu <zxcvb159852@gmail.com>
2023-03-24 18:58:05 +0900

    제발 되셔랑~

--- src/pages/ReviewBoard.js
+++ src/pages/ReviewBoard.js
@@ -1,6 +1,29 @@
+import ReviewSearchFilter from "../components/commons/ReviewSearchFilter";
+import RallyList from "../components/lists/ReviewList";
+import style from "./ReviewBoard.module.css";
+
 function ReviewBoard() {
 
-    return null;
+    return (
+        <main className={style.container}>
+
+            <section className={style.filter}>
+                <ReviewSearchFilter />
+            </section>
+
+            <section className={style.board}>
+                <article className={style.title}>
+                    <h1>랠리 후기</h1>
+                </article>
+
+                <article className={style.list}>
+                    <RallyList />
+                </article>
+
+            </section>
+ 
+        </main>
+    );
 }
 
 export default ReviewBoard;
