921509baff83ac6bf57e8cfb804551710b749af1
migratezuu <zxcvb159852@gmail.com>
2023-04-20 09:39:59 +0900

    [feat]reviewEditPage 제작 및 api 연동

--- src/apis/ReviewAPICalls.js
+++ src/apis/ReviewAPICalls.js
@@ -1,14 +1,16 @@
-import { GET_REVIEW, GET_REVIEWLIST } from '../modules/ReviewModule';
+import { GET_REVIEW, GET_REVIEWLIST, PUT_REVIEW } from '../modules/ReviewModule';
 import { useSelector } from 'react-redux';
 
 // 전체 리뷰 리스트 조회
-export const callReviewListAPI = ({currentPage}) => {
+export const callReviewListAPI = ({ currentPage }) => {
+
+    const token = window.localStorage.getItem('jwtToken');
 
     let URL;
 
-    if(currentPage !== undefined || currentPage !== null){
+    if (currentPage !== undefined || currentPage !== null) {
         URL = `http://localhost:8000/api/v1/reviews?page=${currentPage}`;
-    }else{
+    } else {
         URL = 'http://localhost:8000/api/v1/reviews';
     }
 
@@ -19,48 +21,84 @@ export const callReviewListAPI = ({currentPage}) => {
         const result = await fetch(URL, {
             method: "GET",
             headers: {
-                "Content-type":"application/json",
-                "Accept": "*/*"
+                "Content-type": "application/json",
+                "Accept": "*/*",
+                "Auth": token
             }
         })
-        .then(response => response.json());
+            .then(response => response.json());
 
-        if(result.httpStatus === 200){
-            console.log('[ReviewAPICalls] callReviewListAPI RESULT :', result.results);
-            dispatch({ type: GET_REVIEWLIST, payload: result.results});
+        if (result.httpStatus === 200) {
+            console.log('[ReviewAPICalls] callReviewListAPI RESULT :', result);
+            dispatch({ type: GET_REVIEWLIST, payload: result.results });
         }
-    }
+    };
 }
 
 /* 상세 조회 */
-export const callReviewDetailAPI = ({reviewId}) => {
-    const URL = `api/v1/reviews/${reviewId}`;
+export const callReviewDetailAPI = ({ reviewId }) => {
 
-    
+    const URL = `http://localhost:8000/api/v1/reviews/${reviewId}`;
+
+    const token = window.localStorage.getItem('jwtToken');
+
     return async (dispatch, getState) => {
-        
+
         const result = await fetch(URL, {
             method: "GET",
             headers: {
-                "Content-type":"application/json",
-                "Accept": "*/*"
+                "Content-Type": "application/json",
+                "Accept": "*/*",
+                "Auth": token
             }
         })
-        .then(response => response.json());
-        
-        console.log('[ReviewDetailAPICalls] URL: ', URL);
-        if(result.status === 200){
-            console.log('[ReviewDetailAPICalls] callReviewDetailAPI RESULT :', result);
-            dispatch({ type: GET_REVIEW, payload: result});
+            .then(response => response.json())
+
+        console.log('[ReviewDetailAPICalls] URL: ', result);
+
+        if (result.httpStatus === 200) {
+            console.log('[ReviewDetailAPICalls] callReviewDetailAPI SUCCESS ');
+            dispatch({ type: GET_REVIEW, payload: result.results.reviews });
         }
-        else{
+        else {
             console.log("데이터 안돼");
         }
 
     };
-    
+
 }
 
+/* 데이터 수정 */
+export const callReviewUpdateAPI = ({form, reviewId}) => {
+
+    const URL = `http://localhost:8000/api/v1/reviews/${reviewId}`;
+
+    const token = window.localStorage.getItem('jwtToken');
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(URL, {
+            method: "PUT",
+            headers: {
+                // "Content-Type": 'application/json',
+                "Accept": '*/*',
+                "Auth": token
+            },
+            body: form
+        })
+        // .then(response => response.json());
+
+        console.log('[ReviewAPICalls] callReviewUpdateAPI RESULT : ', result);
+
+        if (result.status === 201) {
+            console.log('[ReviewUpdateAPICalls] callReviewUpdateAPI SUCCESS ');
+            dispatch({ type: PUT_REVIEW, payload: result });
+        }
+        else {
+            console.log("데이터 안돼");
+        };
+    }
+}
 
 
 // export function getRallyReviewDetail(rallyCode){
@@ -72,13 +110,13 @@ export const callReviewDetailAPI = ({reviewId}) => {
 //     return reviews.filter(review => review.reviewId === parseInt(reviewId))[0];
 // }
 
-export function searchReview(reviewTitle){
+export function searchReview(reviewTitle) {
     // return reviews.filter(review => review.reviewname.match(reviewTitle));
 }
 
-export function getReviewList() {
+// export function getReviewList() {
 
-    // const result = [...reviews].reverse();
+//     // const result = [...reviews].reverse();
 
-    // return result;
-}
+//     // return result;
+// }
