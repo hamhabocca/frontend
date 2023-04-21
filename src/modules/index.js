62a036e5352212147a38ad492f563ad4ec4132e2
yuhyunki <christykim47@gmail.com>
2023-04-21 17:10:42 +0900

    [feat & fix] 이것저것 고침

--- src/modules/index.js
+++ src/modules/index.js
@@ -7,10 +7,12 @@ import rallyReducer from "./RallyModule";
 import participateReducer from "./ParticipateModule";
 import reviewReducer from "./ReviewModule";
 import qnaReducer from "./QnaModule";
+import nicknameReducer from "./NicknameModule";
+import listSizeReducer from "./ListSizeModule";
 
 const rootReducer = combineReducers({
     countReducer, addressReducer, modalsReducer, reviewReducer, qnaReducer,
-    rallyReducer, participateReducer, memberReducer
+    rallyReducer, participateReducer, memberReducer, nicknameReducer, listSizeReducer
 });
 
 export default rootReducer;
