247696c97ca2b2a1a9e4fa17df25f6d089804b59
yuhyunki <christykim47@gmail.com>
2023-04-21 17:05:10 +0900

    [style] 불필요한 코드와 공백 삭제

--- src/apis/MemberAPICalls.js
+++ src/apis/MemberAPICalls.js
@@ -1,9 +1,10 @@
-import { async } from "q";
 import { GET_MEMBER } from "../modules/MemberModule";
 import { GET_RALLYLIST } from "../modules/RallyModule";
+import { GET_PARTICIPATE } from "../modules/ParticipateModule";
+import { CHECK_NICKNAME } from "../modules/NicknameModule";
 
 export const getMembers = async () => {
-    
+
     /* 백엔드에 토큰 보내기 */
     const token = window.localStorage.getItem('jwtToken');
 
@@ -19,16 +20,6 @@ export const getMembers = async () => {
     }).then(res => res.json());
 
     console.log('[MemberAPICalls] getMembers RESULT : ', result);
-    if(result.httpStatus === 200){
-        console.log("성공이다~~~~");
-    } else if(result.httpStatus === 401) {
-        
-        console.log("만료됨...");
-
-    }
-    // dispatch({ type: POST_LOGIN,  payload: result });
-
-    console.log(result);
 }
 
 /* 현재 로그인 된 멤버 정보 가져오기 */
@@ -48,15 +39,10 @@ export const getCurrentMember = () => {
                 "Auth": token
             }
         }).then(res => res.json());
-    
+
         console.log('[MemberAPICalls] getCurrentMember RESULT : ', result);
-        if(result.httpStatus === 200){
-            console.log("성공이다~~~~");
-            dispatch({ type: GET_MEMBER,  payload: result.results.member });
-        } else if(result.httpStatus === 401) {
-            
-            console.log("만료됨...");
-    
+        if (result.httpStatus === 200) {
+            dispatch({ type: GET_MEMBER, payload: result.results.member });
         }
     };
 }
@@ -78,15 +64,10 @@ export const getRecruitedRallies = () => {
                 "Auth": token
             }
         }).then(res => res.json());
-    
+
         console.log('[MemberAPICalls] getRecruitedRallies RESULT : ', result);
-        if(result.httpStatus === 200){
-            console.log("성공이다~~~~");
-            dispatch({ type: GET_RALLYLIST,  payload: result.results });
-        } else if(result.httpStatus === 401) {
-            
-            console.log("만료됨...");
-    
+        if (result.httpStatus === 200) {
+            dispatch({ type: GET_RALLYLIST, payload: result.results });
         }
     };
 }
@@ -111,15 +92,124 @@ export const deactivateMember = () => {
             },
         })
         // .then(res => res.json());
-    
+
         console.log('[MemberAPICalls] deactivateMember RESULT : ', result);
-        if(result.httpStatus === 200){
-            console.log("성공이다~~~~");
-            dispatch({ type: GET_RALLYLIST,  payload: result.results });
-        } else if(result.httpStatus === 401) {
-            
-            console.log("만료됨...");
-    
+        if (result.httpStatus === 200) {
+            dispatch({ type: GET_RALLYLIST, payload: result.results });
         }
     };
+}
+
+/* 리뷰 작성 여부 확인 */
+export const checkReviewStatus = (rallyId, memberId) => {
+
+    const token = window.localStorage.getItem('jwtToken');
+
+    const requestURL = ``;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method: 'GET',
+            headers: {
+                "Accept": '*/*',
+                "Auth": token
+            }
+        })
+            .then(res => res.json());
+
+        console.log('[MemberAPICalls] checkReviewStatus RESULT : ', result);
+        if (result.httpStatus === 200) {
+            console.log("찾았다~");
+            dispatch({ type: GET_RALLYLIST, payload: result.results });
+        } else if (result.httpStatus === 401) {
+            console.log("만료됨...");
+        }
+    }
+}
+
+/* 현재 로그인 된 멤버가 참여한 랠리 가져오기 */
+export const getParticipatedRallies = () => {
+
+    const token = window.localStorage.getItem('jwtToken');
+
+    const requestURL = `http://localhost:8000/api/v1/members/participate`
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method: 'GET',
+            headers: {
+                "Content-Type": 'application/json',
+                "Accept": '*/*',
+                "Auth": token
+            }
+        }).then(res => res.json());
+
+        console.log('[MemberAPICalls] getParticipatedRallies RESULT : ', result);
+        if (result.httpStatus === 200) {
+            dispatch({ type: GET_PARTICIPATE, payload: result.results });
+        }
+    };
+}
+
+/* 닉네임 중복 확인 */
+export const checkNickname = (nickname) => {
+
+    const token = window.localStorage.getItem('jwtToken');
+
+    const requestURL = `http://localhost:8000/api/v1/members/duplicate/${nickname}`
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method: 'GET',
+            headers: {
+                "Content-Type": 'application/json',
+                "Accept": '*/*',
+                "Auth": token
+            }
+        })
+            .then(res => res.json());
+
+        console.log('[MemberAPICalls] checkNickname RESULT : ', result);
+        if (result.httpStatus === 200) {
+            dispatch({ type: CHECK_NICKNAME, payload: result.results.result });
+        }
+    }
+}
+
+/* 프로필 수정 */
+export const modifyProfile = ({ form }) => {
+
+    const token = window.localStorage.getItem('jwtToken');
+
+    const memberId = JSON.parse(token).memberId;
+
+    const requestURL = `http://localhost:8000/api/v1/members/${memberId}?type=edit`
+
+    // FormData의 key 확인
+    for (let key of form.keys()) {
+        console.log(key);
+    }
+
+    // FormData의 value 확인
+    for (let value of form.values()) {
+        console.log(value);
+    }
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method: 'PUT',
+            headers: {
+                "Accept": '*/*',
+                "Auth": token
+            },
+            body: form
+        })
+        // .then(res => res.json());
+
+        console.log('[MemberAPICalls] modifyProfile RESULT : ', result);
+    }
 }
