27537d028b68c3619be1aa29eecda21dbad25825
yuhyunki <christykim47@gmail.com>
2023-04-24 18:35:25 +0900

    [fix] console.log제거

--- src/apis/CheckLoginStatusAPICalls.js
+++ src/apis/CheckLoginStatusAPICalls.js
@@ -1,10 +1,4 @@
-import { Navigate, redirect, useNavigate } from "react-router-dom";
-
 export const checkLoginStatusAPICalls = () => {
-
-    console.log("hello")
-
-    console.log(window.localStorage.getItem('jwtToken'));
 
     if(window.localStorage.getItem('jwtToken') == null) {
 
